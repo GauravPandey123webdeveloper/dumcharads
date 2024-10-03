28147f34a32039a2b9e4ad1b96f67b9fc27a8e3f
vikas-chaudhary-2802 <vikasch2802@gmail.com>
2024-10-03 18:07:14 +0530

    Home page and Button

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,20 +1,40 @@
-import React from 'react'
-import style from './Home.module.css'
-import Button from './Button'
+import React, { useRef, useState } from "react";
+import style from "./Home.module.css";
+import { useNavigate } from "react-router-dom";
+import Button from "./Button";
 export default function Home() {
-    const handleSubbmit=()=>{
-       alert("clicked")
+  const [name, setName] = useState("");
+  const inputRef = useRef();
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name.trim()) {
+      inputRef.current.focus();
+      return;
     }
+    localStorage.setItem("name", name);
+    setName("");
+    navigate("/game");
+  };
   return (
+    <div className={style.main}>
+    <div className={style.heading}>Dumcharads....</div>
     <div className={style.home}>
-
       <div className={style.card}>
         <form>
-       
-        
-        <Button name={"Register"} handler={handleSubbmit} defaultStyle={{"color":"red"}}/>
+          <div className={style.usernameLabel}>Username</div>
+          <input
+            type="text"
+            value={name}
+            className={style.usernameInput}
+            onChange={(e) => setName(e.target.value)}
+            ref={inputRef}
+          />
+          <Button name={"Let's Go"} handler={handleSubmit} />
         </form>
       </div>
     </div>
-  )
+    </div>
+    
+  );
 }
